480e48c2d1606b70078ce93cbb28bf54df470d44
Mathieu Abou-Aichi <mathieu.aa@protonmail.com>
2019-10-12 22:04:09 +0200

    Used the new validators in the error checker

--- test/spec/exceptions/entity_validator.spec.js
+++ test/spec/exceptions/entity_validator.spec.js
@@ -19,8 +19,6 @@
 
 const { expect } = require('chai');
 const JDLEntity = require('../../../lib/core/jdl_entity');
-const JDLField = require('../../../lib/core/jdl_field');
-const JDLValidation = require('../../../lib/core/jdl_validation');
 const EntityValidator = require('../../../lib/exceptions/entity_validator');
 
 describe('EntityValidator', () => {
@@ -59,99 +57,6 @@ describe('EntityValidator', () => {
             expect(() => validator.validate({ name: 'A', tableName: 'a' })).to.throw(
               /^The entity attributes name, tableName were not found\.$/
             );
-          });
-        });
-      });
-      context('with fields', () => {
-        context('when there are valid', () => {
-          let entity;
-
-          before(() => {
-            entity = new JDLEntity({
-              name: 'A',
-              tableName: 'a'
-            });
-            const field = new JDLField({
-              name: 'aa',
-              type: 'String'
-            });
-            const validation = new JDLValidation({
-              name: 'required'
-            });
-            field.addValidation(validation);
-            entity.addField(field);
-          });
-
-          it('should not fail', () => {
-            expect(() => validator.validate(entity)).not.to.throw();
-          });
-        });
-        context('when there are not valid', () => {
-          context('because there is no field', () => {
-            let entity;
-
-            before(() => {
-              entity = new JDLEntity({
-                name: 'A'
-              });
-              const field = new JDLField({
-                name: 'aa',
-                type: 'Integer'
-              });
-              entity.addField(field);
-              entity.fields.aa = undefined;
-            });
-
-            it('should fail', () => {
-              expect(() => validator.validate(entity)).to.throw(/^Entity A,\n\tNo field\.$/);
-            });
-          });
-          context('because there is an error in the field', () => {
-            let entity;
-
-            before(() => {
-              entity = new JDLEntity({
-                name: 'A'
-              });
-              const field = new JDLField({
-                name: 'aa',
-                type: 'String'
-              });
-              entity.addField(field);
-              delete entity.fields.aa.type;
-            });
-
-            it('should fail', () => {
-              expect(() => validator.validate(entity)).to.throw(
-                /^Entity A,\n\tThe field attribute type was not found\.$/
-              );
-            });
-          });
-          context('because there is an error in the validation', () => {
-            let entity;
-
-            before(() => {
-              entity = new JDLEntity({
-                name: 'A'
-              });
-              const field = new JDLField({
-                name: 'aa',
-                type: 'String'
-              });
-              const validation = new JDLValidation({
-                name: 'min',
-                value: 42
-              });
-              field.addValidation(validation);
-              entity.addField(field);
-              entity.fields.aa.validations.min.value = undefined;
-            });
-
-            it('should fail', () => {
-              expect(() => validator.validate(entity)).to.throw(
-                /^Entity A,\n\tField aa, \n\tThe validation min requires a value\.$/
-              );
-            });
           });
         });
       });
--- test/spec/exceptions/field_validator.spec.js
+++ test/spec/exceptions/field_validator.spec.js
@@ -19,7 +19,6 @@
 
 const { expect } = require('chai');
 const JDLField = require('../../../lib/core/jdl_field');
-const JDLValidation = require('../../../lib/core/jdl_validation');
 const FieldValidator = require('../../../lib/exceptions/field_validator');
 
 describe('FieldValidator', () => {
@@ -53,54 +52,6 @@ describe('FieldValidator', () => {
       context('when not passing any attribute', () => {
         it('should fail', () => {
           expect(() => validator.validate({})).to.throw(/^The field attributes name, type were not found\.$/);
-        });
-      });
-      context('with validations', () => {
-        context('when there are valid', () => {
-          let field;
-
-          before(() => {
-            field = new JDLField({
-              name: 'a',
-              type: 'String'
-            });
-            const validation = new JDLValidation({
-              name: 'required'
-            });
-            field.addValidation(validation);
-          });
-
-          it('should not fail', () => {
-            expect(() => validator.validate(field)).not.to.throw();
-          });
-        });
-        context('when there are invalid', () => {
-          let field;
-
-          before(() => {
-            field = new JDLField({
-              name: 'a',
-              type: 'String'
-            });
-            const validation1 = new JDLValidation({
-              name: 'maxlength',
-              value: 42
-            });
-            field.addValidation(validation1);
-            const validation2 = new JDLValidation({
-              name: 'minlength',
-              value: 0
-            });
-            field.addValidation(validation2);
-            field.validations.maxlength.value = undefined;
-            field.validations.minlength = undefined;
-          });
-
-          it('should not fail', () => {
-            expect(() => validator.validate(field)).to.throw(
-              /^Field a, \n\tThe validation maxlength requires a value\.\n\tNo validation\.$/
-            );
-          });
         });
       });
     });
